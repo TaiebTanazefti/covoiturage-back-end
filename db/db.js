1b167e6ed7d3d0c71872f8f9bce727a20e7050d6
TaiebTanazefti <taieb.tanazefti@gmail.com>
2026-02-28 10:20:03 -0500

    Ajout de la gestion des préférences utilisateur, permettant la personnalisation des notifications et des paramètres de compte. Amélioration de l'interface utilisateur pour une meilleure expérience lors de la gestion des réservations.

--- db/db.js
+++ db/db.js
@@ -13,39 +13,47 @@ let db = await open({
     filename: process.env.DB_FILE,
     driver: sqlite3.Database
 });
-
+await db.exec('PRAGMA foreign_keys = ON;');
 // Création de la table si elle n'existe pas, on 
 // peut écrire du code SQL pour initialiser les 
 // tables et données dans la fonction exec()
+// statuts possibles de reservation : EN_ATTENTE/ ACCEPTEE/ REFUSEE/ ANNULEE
+// status possibles de trajet : ACTIF / ANNULE
 if(IS_NEW) {
     await db.exec(`
         CREATE TABLE utilisateur(
         id INTEGER PRIMARY KEY,
-        courriel TEXT NOT NULL,
+        courriel TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
         nom TEXT NOT NULL,
         prenom TEXT NOT NULL,
         role TEXT NOT NULL,
         telephone INTEGER NOT NULL,
-        note INTEGER NOT NULL,
-        nbreDeNotes INTEGER NOT NULL
+        note INTEGER  NOT NULL DEFAULT 0,
+        nbreDeNotes INTEGER NOT NULL DEFAULT 0,
+        valide INTEGER NOT NULL DEFAULT 0,
+        actif INTEGER NOT NULL DEFAULT 1
         );
 
+        
         CREATE TABLE reservation(
         id INTEGER PRIMARY KEY,
         statut TEXT NOT NULL,
+        trajet_id INTEGER NOT NULL,
+        utilisateur_id INTEGER NOT NULL,
         FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id),
         FOREIGN KEY (trajet_id) REFERENCES trajet(id)
 
         );
 
         CREATE TABLE trajet(
-        id INTEGER PRIMARY KEY
+        id INTEGER PRIMARY KEY,
         pointDeDepart TEXT NOT NULL,
         pointDarrivee TEXT NOT NULL,
         dateEtHeure DATETIME NOT NULL,
         nombreDePlacesDisponibles INTEGER NOT NULL,
-        statut TEXT NOT NULL,
+        statut TEXT NOT NULL DEFAULT 'ACTIF',
+        utilisateur_id INTEGER NOT NULL,
         FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
 
         );
